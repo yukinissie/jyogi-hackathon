28ba523cd1ea1760796684af97a06ff155823b47
yukinissie <bigtree2490@outlook.jp>
2025-05-25 09:08:02 +0900

    react-image-renderer: 本番用に画像URLを差し替え

--- react-image-renderer/src/app/page.tsx
+++ react-image-renderer/src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center min-h-screen">
       <img
-        src="https://sample-portfolio.yukinissie.com/images/sample-og-image.jpg"
-        alt="Next.js logo"
+        src="https://sample-portfolio.yukinissie.com/images/image1.png"
+        alt="1コマ目の画像"
         data-testid="home-image"
       />
     </main>
